bd4152c0c307697397491102b8be12633510e9d3
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2023-02-16 13:14:24 +0200

    bits of document DSL in tested module

--- ruby-sandbox/build_dsl_attempt.rb
+++ ruby-sandbox/build_dsl_attempt.rb
@@ -1,1 +1,19 @@
+module DocumentDsl
+  def document(attrs)
+    { family: "flow" }
+      .merge(attrs)
+      .merge({ applications: yield([]) })
+  end
 
+  def application(attrs)
+    attrs.merge({ services: yield([]) })
+  end
+
+  def service(attrs)
+    attrs.merge({ structures: yield([]) })
+  end
+
+  def structure(attrs)
+    attrs
+  end
+end
--- ruby-sandbox/build_dsl_attempt_spec.rb
+++ ruby-sandbox/build_dsl_attempt_spec.rb
@@ -1,1 +1,41 @@
+require "minitest/autorun"
+require "minitest/pride"
 
+#load relatives from current dir
+$LOAD_PATH << "."
+require "build_dsl_attempt"
+
+class DocumentDslTest < Minitest::Test
+  include DocumentDsl
+
+  def test_assemble_empty_doc
+    integration_type = { name: "API", deprecated: false }
+
+    document = document(author: "test-author", name: "test-name") do |children|
+      children << application(name: "app-name", spi: "SP-SP") do |app_child|
+        app_child << service(name: "service-name", integration_type: integration_type) do |service_child|
+          service_child << structure(name: "ABC", type: "SQL")
+          service_child << structure(name: "ABC", type: "SQL")
+        end
+      end
+    end
+
+    assert_equal document, {
+      family: "flow", author: "test-author", name: "test-name",
+      applications: [
+        {
+          name: "app-name", spi: "SP-SP",
+          services: [
+            {
+              name: "service-name", integration_type: integration_type,
+              structures: [
+                { name: "ABC", type: "SQL" },
+                { name: "ABC", type: "SQL" },
+              ],
+            },
+          ],
+        },
+      ],
+    }
+  end
+end
